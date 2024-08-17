c106fb3943b8cb2a2739a343a7d4123cd57946f1
mohamednasser2001 <mmm.zzz155mody@gmail.com>
2024-08-17 13:12:50 +0300

    first commit

---
+++ task se one/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace task_se_one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("number of small carpes:");
+            int smaalCarpets = int.Parse(Console.ReadLine());
+            Console.WriteLine("number of larg carpes:");
+            int largCarpets = int.Parse(Console.ReadLine());
+            Console.WriteLine("price per small carpets: $25");
+            Console.WriteLine("price per larg carpets: $35");
+            int cost = (smaalCarpets * 25) + (largCarpets * 35);
+            Console.WriteLine($"cost:{cost}");
+            Console.WriteLine("tax:$6.6");
+            double tax = 6.0 / 100 * cost ;
+            double totalEstimate = tax + cost;
+            Console.WriteLine($"total estimate:${totalEstimate}");
+            Console.WriteLine("this estimate is valid for 30 days");
+
+        }
+    }
+}
